1efccb77f5d59f5426623b8617718bab7fee277b
tomilovsigors-beep <tomilovsigors@gmail.com>
2026-09-17 00:00:17 +0300

    product xml: check Drive API transport for proposal

--- product-xml-runtime/gunicorn.conf.py
+++ product-xml-runtime/gunicorn.conf.py
@@ -9,10 +9,10 @@ def on_starting(server):
             server.log.info('GOOGLE_SERVICE_ACCOUNT_PRINCIPAL %s', email)
         from google.oauth2 import service_account
         from google.auth.transport.requests import AuthorizedSession
-        creds=service_account.Credentials.from_service_account_info(info,scopes=['https://www.googleapis.com/auth/cloud-platform'])
+        creds=service_account.Credentials.from_service_account_info(info,scopes=['https://www.googleapis.com/auth/drive'])
         s=AuthorizedSession(creds)
-        u='https://serviceusage.googleapis.com/v1/projects/458713973345/services/sheets.googleapis.com'
+        u='https://www.googleapis.com/drive/v3/files/1xBVjjcLYqiQvy2nLtl-tGt8w_7FWefWxFa2Ltthq-7I?fields=id,name,mimeType,capabilities'
         r=s.get(u,timeout=30)
-        server.log.info('GOOGLE_SERVICE_USAGE_DIAG status=%s body=%s',r.status_code,(r.text or '')[:1400].replace('\n',' '))
+        server.log.info('GOOGLE_DRIVE_API_DIAG status=%s body=%s',r.status_code,(r.text or '')[:1400].replace('\n',' '))
     except Exception as exc:
-        server.log.warning('GOOGLE_SERVICE_USAGE_DIAG_ERROR %s %s', type(exc).__name__, str(exc)[:500])
+        server.log.warning('GOOGLE_DRIVE_API_DIAG_ERROR %s %s', type(exc).__name__, str(exc)[:500])
